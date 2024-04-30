bc76647447ea8f621099c2a2fc63df9c602681cf
Дмитрий Сорокин <isip_d.m.sorokin@mpt.ru>
2024-04-30 13:19:40 +0300

    api_some_change_5

--- run.py
+++ run.py
@@ -6,32 +6,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-# class User():
-#     def __init__(self, name, username):
-#         self.name = name
-#         self.username = username
-#     def __init__(self, name):
-#         self.name = name
-#         self.username = "NONE"
-#     # def __init__(self, username):
-#     #     self.name = "NONE"
-#     #     self.username = username
-
-# import json
-
-# def add_element(j, name, value):
-#     j[name] = value
-
-# j1 = json.loads(
-# """
-# {
-# "username": "jhon"
-# }
-# """)
-
-# print(j1)
-# add_element(j1, "name", "jhonatan")
-# print(j1)
