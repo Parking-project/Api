292d1415377f1b637bc8c86be7c2d8ba123bfd8b
Дмитрий Сорокин <isip_d.m.sorokin@mpt.ru>
2024-05-01 20:02:10 +0300

    api_domain

---
+++ core/domain/Interface/__init__.py
@@ -0,0 +1,25 @@
+__all__ = (
+    "IWPAuthHistory",
+    "IWPDocument",
+    "IWPMessage",
+    "IWPMessageMeta",
+    "IWPPlace",
+    "IWPReserve",
+    "IWPReserveHistory",
+    "IWPRole",
+    "IWPTokenBlocList",
+    "IWPUser",
+    "IWPUserMeta"
+)
+
+from .IWPAuthHistory import IWPAuthHistory
+from .IWPDocument import IWPDocument
+from .IWPMessage import IWPMessage
+from .IWPMessageMeta import IWPMessageMeta
+from .IWPPlace import IWPPlace
+from .IWPReserve import IWPReserve
+from .IWPReserveHistory import IWPReserveHistory
+from .IWPRole import IWPRole
+from .IWPTokenBlocList import IWPTokenBlocList
+from .IWPUser import IWPUser
+from .IWPUserMeta import IWPUserMeta
