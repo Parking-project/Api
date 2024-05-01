d8fa49b3a8a9cd078814b3fde46af8ce771036db
Дмитрий Сорокин <isip_d.m.sorokin@mpt.ru>
2024-05-01 19:58:38 +0300

    api_shared

---
+++ apps/shared/__init__.py
@@ -0,0 +1,15 @@
+__all__ = (
+    "UniqueError", 
+    "PageError", 
+    "ConnectionError",
+    "SecurityError",
+    "DataError"
+)
+
+from .global_exception import (
+    UniqueError, 
+    PageError, 
+    ConnectionError,
+    SecurityError,
+    DataError
+)
